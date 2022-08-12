07a1c61a690791a29bd5868865814bbacf8f58a2
Josh Marinacci <joshua@marinacci>
2022-08-12 13:30:38 -0400

    more work to add dialogs

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './App.css';
-import {HBox, VBox, Spacer} from "josh_react_util";
+import {HBox, Spacer,
+    DialogContext, DialogContainer,
+    sampleDialogContext
+} from "josh_react_util";
 
-function App() {
+function Content():JSX.Element {
+    let dm = useContext(DialogContext)
+    console.log("dm is",dm)
+    return <div>
+    <HBox>
+        <button>hi</button>
+        <Spacer></Spacer>
+        <button onClick={()=>{
+            dm.show(<button>I'm a dialog</button>)
+        }}>there</button>
+    </HBox>
+    </div>
+}
+function App():JSX.Element {
   return (
-      <HBox>
-          <button>hi</button>
-          <Spacer></Spacer>
-          <button>there</button>
-      </HBox>
+      <DialogContext.Provider value={sampleDialogContext}>
+          <Content/>
+          <DialogContainer/>
+      </DialogContext.Provider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React, {ReactNode} from "react"
+import React, {createContext, ReactNode, useContext, useEffect, useState} from "react"
 
 import "./index.css"
 
 /*
 - [ ] simple Dark and light themes with css variables
-- [ ] vbox and hbox with child prop propagation
+- [x] vbox and hbox with child prop propagation
 - [ ] main fill panel to completely fill the browser
 - [ ] popup and dialog
 - [ ] properly scaled canvas with a drawing delegate
@@ -13,7 +13,6 @@ import "./index.css"
 - [ ] verify I can import it into another project
 
  */
-
 export function Spacer() {
     return <span className={"spacer"}/>
 }
@@ -25,9 +24,56 @@ export function toClass(param: object):string {
 }
 
 export function HBox(props: { children: ReactNode, className?:string }) {
-    return <div className={"hbox " + props.className} {...props}>{props.children}</div>
+    return <div className={"hbox " + props.className?props.className:""} {...props}>{props.children}</div>
 }
 
 export function VBox(props: { children: ReactNode, className?:string }) {
-    return <div className={"vbox " + props.className} {...props}>{props.children}</div>
+    return <div className={"vbox " + props.className?props.className:""} {...props}>{props.children}</div>
+}
+
+
+type ShowDialogType = (view:JSX.Element|null, visible:boolean) => void
+interface DialogContextInterface {
+    show(view: JSX.Element): void
+    hide():void
+    on_change(cb:ShowDialogType): void
+}
+export class DialogContextImpl implements DialogContextInterface {
+    private listeners: ShowDialogType[];
+    constructor() {
+        this.listeners = []
+    }
+    hide(): void {
+        this.listeners.forEach(cb => cb(null,false))
+    }
+
+    on_change(cb:ShowDialogType): void {
+        this.listeners.push(cb)
+    }
+
+    show(view: JSX.Element): void {
+        this.listeners.forEach(cb => cb(view,true))
+    }
+
+}
+export const sampleDialogContext: DialogContextInterface = new DialogContextImpl()
+export const DialogContext = createContext<DialogContextInterface>(sampleDialogContext);
+
+export function DialogContainer() {
+     let dm = useContext(DialogContext)
+    // @ts-ignore
+    let [content, set_content] = useState<JSX.Element|null>(null)
+    useEffect(()=>{
+        dm.on_change(v => {
+            console.log("changing dialog")
+            set_content(v)
+        })
+    })
+    return <div className={toClass({
+        dialogWrapper:true,
+        visible:true,
+    })}>
+        <span>dialog is here</span>
+        {content?content:"no content"}
+    </div>
 }
