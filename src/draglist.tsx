7e607f3e252435f7a6f4c1b76a2dd67834d83cae
Josh Marinacci <joshua@marinacci.org>
2025-05-21 16:12:21 -0700

    drag lists

--- src/draglist.tsx
+++ src/draglist.tsx
@@ -2,108 +2,113 @@ import "./draglist.css"
 import {MouseEvent, useRef} from "react"
 const log = (...args:any[]) => console.log("",...args);
 
+class DragState {
+    private list: HTMLElement
+    private dragTarget: HTMLElement|null = null
+    private timer:number|null
+    private clientY: number
 
-let dragTarget:HTMLElement|null
-let clientY:number
-let timer:number|null
-
-function checkBounds(dragTarget:HTMLElement, list:HTMLElement) {
-    const list_bounds = list.getBoundingClientRect()
-    const dragtarget_bounds = dragTarget.getBoundingClientRect()
-    const target_y = dragtarget_bounds.top - list_bounds.top
-    if(target_y < 0) {
-        list.scrollTop = Math.max(list.scrollTop - 5,0);
+    constructor(list:HTMLElement, dragTarget:HTMLElement) {
+        this.list = list
+        this.dragTarget = dragTarget
+        this.timer = null
+        this.clientY = 0
     }
-    if(target_y + dragtarget_bounds.height > list.clientHeight) {
-        list.scrollTop = Math.min(list.scrollTop + 5, list.clientHeight);
+
+    checkBounds() {
+        if(!this.dragTarget) return
+        const list_bounds = this.list.getBoundingClientRect()
+        const dragtarget_bounds = this.dragTarget.getBoundingClientRect()
+        const target_y = dragtarget_bounds.top - list_bounds.top
+        if(target_y < 0) {
+            this.list.scrollTop = Math.max(this.list.scrollTop - 5,0);
+        }
+        if(target_y + dragtarget_bounds.height > this.list.clientHeight) {
+            this.list.scrollTop = Math.min(this.list.scrollTop + 5, this.list.clientHeight);
+        }
+        this.dragTarget.style.top = (this.clientY - list_bounds.top + this.list.scrollTop - 10) + "px";
     }
-    dragTarget.style.top = (clientY - list_bounds.top + list.scrollTop - 10) + "px";
-}
 
-function startDrag(ul:HTMLElement, li:HTMLElement, cy:number) {
-    log('start drag')
-    dragTarget = li;
-    dragTarget.classList.add('dragging')
-    clientY = cy
-    timer = setInterval(() => {
-        checkBounds(dragTarget as HTMLElement, ul)
-    },10)
-}
-
-function moveDrag(ul:HTMLElement, cy:number) {
-    if(!dragTarget) return;
-    log('move drag')
-    const list = ul
-    const list_bounds = list.getBoundingClientRect()
-    clientY = cy
-    for(let i = 0; i < ul.children.length; i++) {
-        const li = ul.children[i] as HTMLElement
-        if(li === dragTarget) {
-            dragTarget.style.left = '0px'
-            dragTarget.style.top = (cy - list_bounds.top + list.scrollTop - 10) + "px";
-        } else {
-            const bounds = li.getBoundingClientRect()
-            const half = bounds.height / 2
-            if (bounds.bottom - half < clientY) {
+    startDrag(cy:number) {
+        log('start drag')
+        this.dragTarget.classList.add('dragging')
+        this.clientY = cy
+        this.timer = setInterval(() => {
+            this.checkBounds()
+        },10)
+    }
+    startMouseDrag(clientY:number) {
+        log('start mouse drag')
+        this.startDrag(clientY)
+        const handle_mouse_move = (e) => {
+            if(!this.dragTarget) return;
+            log("mouse move")
+            e.preventDefault()
+            e.stopPropagation()
+            this.moveDrag(e.clientY)
+        }
+        const handle_mouse_end = (e) => {
+            log("mouse end")
+            this.stopDrag()
+            window.removeEventListener('mousemove',handle_mouse_move)
+            window.removeEventListener('mouseup', handle_mouse_end)
+        }
+        window.addEventListener('mousemove', handle_mouse_move)
+        window.addEventListener("mouseup", handle_mouse_end)
+    }
+    moveDrag(cy:number) {
+        if(!this.dragTarget) return;
+        log('move drag')
+        const list_bounds = this.list.getBoundingClientRect()
+        this.clientY = cy
+        for(let i = 0; i < this.list.children.length; i++) {
+            const li = this.list.children[i] as HTMLElement
+            if(li === this.dragTarget) {
+                this.dragTarget.style.left = '0px'
+                this.dragTarget.style.top = (cy - list_bounds.top + this.list.scrollTop - 10) + "px";
+            } else {
+                const bounds = li.getBoundingClientRect()
+                const half = bounds.height / 2
+                if (bounds.bottom - half < this.clientY) {
+                    li.style.transform = 'translateY(0)'
+                }
+                if (bounds.top + half > this.clientY) {
+                    li.style.transform = 'translateY(100%)'
+                }
+            }
+        }
+    }
+    stopDrag() {
+        log('stop drag')
+        if (this.dragTarget) {
+            if (this.timer) clearInterval(this.timer)
+            this.timer = null
+            this.dragTarget.classList.remove('dragging')
+            this.dragTarget = null
+            this.clientY = -1
+            for(let i = 0; i < this.list.children.length; i++) {
+                const li = this.list.children[i] as HTMLElement
                 li.style.transform = 'translateY(0)'
             }
-            if (bounds.top + half > clientY) {
-                li.style.transform = 'translateY(100%)'
-            }
         }
     }
-}
-
-function stopDrag(ul:HTMLElement) {
-    // dragging = false
-    log('stop drag')
-    if (dragTarget) {
-        if (timer) clearInterval(timer)
-        timer = null
-        dragTarget.classList.remove('dragging')
-        dragTarget = null
-        clientY = -1
-        for(let i = 0; i < ul.children.length; i++) {
-            const li = ul.children[i] as HTMLElement
-            li.style.transform = 'translateY(0)'
-        }
-    }
-}
-
-function startMouseDrag(ul:HTMLElement, li:HTMLElement, clientY:number) {
-    log('start mouse drag')
-    startDrag(ul, li, clientY)
-    const handle_mouse_move = (e) => {
-        if(!dragTarget) return;
-        log("mouse move")
-        e.preventDefault()
-        e.stopPropagation()
-        moveDrag(ul, e.clientY)
-    }
-    const handle_mouse_end = (e) => {
-        log("mouse end")
-        stopDrag(ul)
-        window.removeEventListener('mousemove',handle_mouse_move)
-        window.removeEventListener('mouseup', handle_mouse_end)
-    }
-    window.addEventListener('mousemove', handle_mouse_move)
-    window.addEventListener("mouseup", handle_mouse_end)
 }
 
 export function DragListDemo() {
-    const ref = useRef<HTMLUListElement|null>(null)
+    const listRef = useRef<HTMLUListElement|null>(null)
     const onMouseDown = (e:MouseEvent<HTMLElement>) => {
         const span = e.target as HTMLElement
         const li = span.parentElement as HTMLElement
         const clientY = e.clientY
-        startMouseDrag(ref.current as HTMLElement, li, clientY)
+        let ds = new DragState(listRef.current as HTMLElement, li)
+        ds.startMouseDrag(clientY)
     }
 
     const props = {
         onMouseDown: onMouseDown,
     }
 
-    return <ul ref={ref} style={{
+    return <ul ref={listRef} style={{
         maxHeight: '150px',
         overflow: 'scroll',
     }}>
@@ -118,64 +123,3 @@ export function DragListDemo() {
         <li>Drag Item eight <span {...props}>D</span></li>
     </ul>
 }
-
-
-/*
-
-    let dragTarget = null;
-    let dragging = false;
-    let timer = null;
-    let clientY = -1
-    let hold = null
-
-    if(target_y + dragtarget_bounds.height > list.clientHeight) {
-    list.scrollTop = Math.min(list.scrollTop + 5, list.clientHeight);
-}
-    dragTarget.style.top = (clientY - list_bounds.top + list.scrollTop - 10) + "px";
-}
-
-}
-}
-}
-
-function startTouchDrag(li, clientY) {
-    log('start touch drag')
-    startDrag(li, clientY)
-    const handle_touch_move = (e) => {
-        if(!dragTarget) return;
-        log('touch move')
-        e.preventDefault()
-        e.stopPropagation()
-        moveDrag(e.touches[0].clientY)
-    }
-    const handle_touch_end = (e) => {
-        stopDrag()
-        log('touch end')
-        window.removeEventListener("touchmove", handle_touch_move, { passive: false})
-        window.removeEventListener("touchend", handle_touch_end)
-    }
-    window.addEventListener("touchmove", handle_touch_move, {  passive: false })
-    window.addEventListener("touchend", handle_touch_end)
-}
-
-function startMouseDrag(li, clientY) {
-    log('start mouse drag')
-    startDrag(li, clientY)
-    const handle_mouse_move = (e) => {
-        if(!dragTarget) return;
-        log("mouse move")
-        e.preventDefault()
-        e.stopPropagation()
-        moveDrag(e.clientY)
-    }
-    const handle_mouse_end = (e) => {
-        log("mouse end")
-        stopDrag()
-        window.removeEventListener('mousemove',handle_mouse_move)
-        window.removeEventListener('mouseup', handle_mouse_end)
-    }
-    window.addEventListener('mousemove', handle_mouse_move)
-    window.addEventListener("mouseup", handle_mouse_end)
-}
-
- */
