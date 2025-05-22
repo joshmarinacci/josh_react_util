a02df210bf442760310ff4dc1fd372491da4fdea
Josh Marinacci <joshua@marinacci.org>
2025-05-22 11:03:02 -0700

    more work on inline emojis

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import {TabbedPanel} from "./tabbedpanel";
 import {HBox, VBox} from "./comps";
 import {PopupContainer, PopupContext, PopupContextImpl} from "./popup";
-import {useContext, useState} from "react";
+import {useContext, useRef, useState} from "react";
 import "./index.css"
 import {DialogContainer, DialogContext, DialogContextImpl} from "./dialog";
 import {DragListDemo} from "./draglist";
+import {toClass} from "./util";
 
 function PopupContent() {
     const pm = useContext(PopupContext);
@@ -24,10 +25,115 @@ function PopupContent() {
     </div>
 }
 
+const completions = [
+    "smile",
+    "happy",
+    "explode",
+]
+const emojis = {
+    "smile": "😀",
+    "happy": "😃",
+    "explode": "💥",
+}
+
+interface TextPopupProps {
+    value: string,
+    selected: string,
+    elements: string[]
+}
+
+function TextPopup({value, selected, elements}: TextPopupProps) {
+    return <ul style={{
+        display: "flex",
+        flexDirection: "column",
+        border: '1px solid red',
+    }}>
+        {elements.map((item, index) => {
+            return <li key={index} className={toClass({
+                selected: selected === item
+            })}>{item} {emojis[item]}</li>
+        })}
+    </ul>
+}
+
+function CustomInput() {
+    const [text, setText] = useState("");
+    const [show, setShow] = useState(false);
+    const [selected, setSelected] = useState("smile");
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    let value = ""
+    if(inputRef.current) {
+        console.log("selection is", inputRef.current.selectionStart, inputRef.current.selectionEnd)
+        let n = text.lastIndexOf(":")
+        if (n >= 0) {
+            value = text.substring(n + 1)
+        }
+    }
+    console.log("value is ", value)
+
+    let elements = completions.filter(t => {
+        if (!value) return true
+        if (value.trim() === "") return true
+        // console.log("checking ", t, value.trim())
+        if (t.startsWith(value.trim())) return true
+        return false
+    })
+
+    const moveSelectionDown = () => {
+        let n = elements.indexOf(selected) + 1
+        if (n >= elements.length) n = 0
+        setSelected(elements[n])
+    }
+    const moveSelectionUp = () => {
+        let n = elements.indexOf(selected) - 1
+        if (n < 0) n = elements.length - 1
+        setSelected(elements[n])
+    }
+
+
+    return <div><input type='text' value={text} ref={inputRef}
+                       onKeyDown={(e) => {
+                           // console.log("pressed ", e.key)
+                           if (e.key === ':') {
+                               setShow(true);
+                           }
+                           if (e.key === 'ArrowDown') {
+                               e.preventDefault()
+                               moveSelectionDown()
+                           }
+                           if (e.key === 'ArrowUp') {
+                               e.preventDefault()
+                               moveSelectionUp()
+                           }
+                           if (e.key === 'Enter') {
+                               setShow(false);
+                               let n = text.lastIndexOf(":")
+                               if (n >= 0) {
+                                   let before = text.substring(0, n)
+                                   setText(before + emojis[selected])
+                               }
+                           }
+                           if (e.key === 'Escape') {
+                               e.preventDefault()
+                               setShow(false);
+                           }
+                           if (e.key === 'Tab') {
+                               e.preventDefault()
+                           }
+                       }}
+                       onChange={(e) => {
+                           setText(e.target.value)
+                       }}/>
+        {show && <TextPopup value={value} selected={selected} elements={elements}/>}
+    </div>
+}
+
 function PopupDemo() {
     const pm = useContext(PopupContext);
     return <VBox>
-        <input type={'text'}/>
+        <CustomInput/>
         <HBox>
             <button>hbox</button>
             <button>hbox</button>
@@ -43,7 +149,8 @@ function PopupDemo() {
         <HBox>
             <button
                 onClick={(e) => pm.show_at(<PopupContent/>, e.target)}
-            >show popup</button>
+            >show popup
+            </button>
         </HBox>
     </VBox>
 }
@@ -57,7 +164,7 @@ function TestDialog() {
         <section>
             <input type="text" value={text} onChange={(e) => setText(e.target.value)}
                    onKeyDown={e => {
-                       if(e.key === "Enter") {
+                       if (e.key === "Enter") {
                            e.preventDefault();
                            e.stopPropagation();
                            dm.hide()
@@ -65,7 +172,9 @@ function TestDialog() {
                    }}
             />
         </section>
-        <footer><button className={'primary'} onClick={() => dm.hide()}>dismiss</button></footer>
+        <footer>
+            <button className={'primary'} onClick={() => dm.hide()}>dismiss</button>
+        </footer>
     </dialog>
 }
 
@@ -80,7 +189,7 @@ export function App() {
     return <div className="debug">
         <DialogContext.Provider value={new DialogContextImpl()}>
             <PopupContext.Provider value={new PopupContextImpl()}>
-                <TabbedPanel titles={['Inputs', 'Layout', 'Dialog','Drag List']}>
+                <TabbedPanel titles={['Inputs', 'Layout', 'Dialog', 'Drag List']}>
                     <VBox className={'show-focus-within'}>
                         <PopupDemo/>
                     </VBox>
